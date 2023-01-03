258ae59b80bb89d383d959423582c46cb6e2e4e9
zzzdks760 <zzzdks760@naver.com>
2023-01-03 21:54:36 +0900

    구현 알고리즘

---
+++ SungMin/BOJ/10798.py
@@ -0,0 +1,18 @@
+words = []
+li = []
+
+for i in range(5):
+    word = input()
+    words.append(word)
+    li.append(len(word))
+
+
+res = ''
+for i in range(max(li)):
+    for j in range(5):
+        if i < li[j]:
+            res += words[j][i]
+print(li)
+print(res)
+
+
